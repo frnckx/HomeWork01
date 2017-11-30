084641b94dc310c42f936b9bded9f18be46ee8bc
frnckx <jakab.fruzsi@gmail.com>
2017-11-30 19:43:15 +0100

    #1

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,14 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+    /* Deklaráltam két int típusú változót, és ezzel az értékkel inicializáltam
+ a perimeter és az area változókat, és kiírattam a konzolra */
+        int numberA = 5;
+        int numberB = 6;
+        int perimeter = 2*(numberA+numberB);
+        int area = numberA*numberB;
+        System.out.println("A téglalap kerülete: " + perimeter);
+        System.out.println("A téglalap területe: " + area);
     }
-}
+    }
+
